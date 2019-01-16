4d8599ad95528ed1479c4ae95ac2cf8702b96619
xivk <ben@itinero.tech>
2019-01-16 12:46:58 +0000

    Handle proxy headers.

--- src/RouteableTiles.API/Startup.cs
+++ src/RouteableTiles.API/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -42,6 +43,36 @@ namespace RouteableTiles.API
             {
                 app.UseDeveloperExceptionPage();
             }
+            
+            var options = new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedHost | ForwardedHeaders.XForwardedProto
+            };
+            options.KnownNetworks.Clear();
+            options.KnownProxies.Clear();
+            
+            app.UseForwardedHeaders(options);
+            app.Use((context, next) => 
+            {
+                if (context.Request.Headers.TryGetValue("X-Forwarded-PathBase", out var pathBases))
+                {
+                    context.Request.PathBase = pathBases.First();
+                    if (context.Request.PathBase.Value.EndsWith("/"))
+                    {
+                        context.Request.PathBase =
+                            context.Request.PathBase.Value.Substring(0, context.Request.PathBase.Value.Length - 1);
+                    }
+                    if (context.Request.Path.Value.StartsWith(context.Request.PathBase.Value))
+                    {
+                        var before = context.Request.Path.Value;
+                        var after = context.Request.Path.Value.Substring(
+                            context.Request.PathBase.Value.Length,
+                            context.Request.Path.Value.Length - context.Request.PathBase.Value.Length);
+                        context.Request.Path = after;
+                    }
+                }
+                return next();
+            });
             
             DatabaseInstance.Default = new Database(this.Configuration["db"], 14);
 
