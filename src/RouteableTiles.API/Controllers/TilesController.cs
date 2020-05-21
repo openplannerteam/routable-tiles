09879da45c15c0211e2fa0ac8063b051602366e2
xivk <ben@itinero.tech>
2020-05-21 16:37:47 +0100

    Implemented memento basics.

--- src/RouteableTiles.API/Controllers/TilesController.cs
+++ src/RouteableTiles.API/Controllers/TilesController.cs
@@ -1,11 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Net.Http.Headers;
 using RouteableTiles.API.Responses;
 using RouteableTiles.IO.JsonLD.Semantics;
 using RouteableTiles.IO.JsonLD.Tiles;
+using Serilog;
 
 namespace RouteableTiles.API.Controllers
 {
+    /// <summary>
+    /// Controller to return routeable tiles.
+    /// </summary>
+    /// <remarks>
+    /// This implements the memento: http://www.mementoweb.org/guide/howto/
+    ///
+    /// The generic URI: {z}/{x}/{y} will redirect to the latest version URI.
+    /// The version URI: {timestamp}/{z}/{x}/{y}
+    /// 
+    /// </remarks>
     [Route("/")]
     [ApiController]
     public class TilesController : ControllerBase
@@ -16,16 +30,60 @@ namespace RouteableTiles.API.Controllers
             var db = DatabaseInstance.Default;
             
             if (db == null) return NotFound();
-            if (db.Latest == null) return NotFound();
-            if (z != db.Latest.Zoom) return NotFound();
+            
+            var latest = db.Latest;
+            if (latest == null) return NotFound();
+            if (z != latest.Zoom) return NotFound();
+            var date = latest.EndTimestamp;
+            
+            if (this.Request.Headers.TryGetValue("Accept-Datetime", out var value) &&
+                DateTime.TryParseExact(value.ToString(), "ddd, dd MMM yyyy HH:mm:ss G\\MT", System.Globalization.CultureInfo.InvariantCulture,
+                    DateTimeStyles.AssumeUniversal, out var utcDate))
+            {
+                utcDate = utcDate.ToUniversalTime();
 
-            var tile = new Tile(x, y, z);
-            var data = db.Latest.GetRouteableTile((x, y), (ts) => ts.IsRelevant(
+                var dbAtDate = db.GetOn(utcDate);
+                if (dbAtDate == null) return NotFound();
+                if (z != dbAtDate.Zoom) return NotFound();
+                date = dbAtDate.EndTimestamp;
+            }
+            
+            var timestamp =
+                $"{date.Year:0000}{date.Month:00}{date.Day:00}-{date.Hour:00}{date.Minute:00}{date.Second:00}";
+
+            var routeValues = new RouteValueDictionary {["timestamp"] = timestamp, ["x"] = x, ["y"] = y, ["z"] = z};
+            return new RedirectToActionResult(nameof(GetJsonLDVersion), "Tiles", routeValues);
+        }
+        
+        [HttpGet("{timestamp}/{z}/{x}/{y}/")]
+        public object GetJsonLDVersion(string timestamp, uint z, uint x, uint y)
+        {
+            if (!DateTime.TryParseExact(timestamp, "yyyyMMdd-HHmmss", System.Globalization.CultureInfo.InvariantCulture,
+                DateTimeStyles.AssumeUniversal, out var utcDate))
+            {
+                return NotFound();
+            }
+
+            utcDate = utcDate.ToUniversalTime();
+            
+            var db = DatabaseInstance.Default;
+            if (db == null) return NotFound();
+
+            var instance = db.GetOn(utcDate);
+            if (instance == null) return NotFound();
+            if (z != instance.Zoom) return NotFound();
+            
+            var data = instance.GetRouteableTile((x, y), (ts) => ts.IsRelevant(
                 JsonLDTileResponseFormatter.MappingKeys ?? TagMapper.DefaultMappingKeys, 
                 JsonLDTileResponseFormatter.Mapping ?? TagMapper.DefaultMappingConfigs));
             
+            var baseUrl = $"{this.Request.HttpContext.Request.BasePath()}/{z}/{x}/{y}/";
+            
             Response.Headers[HeaderNames.CacheControl] = "public,max-age=" + 60;
+            Response.Headers["Memento-DateTime"] = utcDate.ToString("ddd, dd MMM yyyy HH:mm:ss G\\MT");
+            Response.Headers["Link"] = $"<{baseUrl}>; rel=\"original\"";
 
+            var tile = new Tile(x, y, z);
             return new OsmTileResponse(data, tile);
         }
 
