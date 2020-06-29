cabab8cfd9d8e8f4cbcdea4ce11bb147972e431f
xivk <ben@itinero.tech>
2020-06-29 10:47:30 +0100

    Added full osm-xml response with memento support.

--- src/RouteableTiles.API/OsmDbExtensions.cs
+++ src/RouteableTiles.API/OsmDbExtensions.cs
@@ -15,5 +15,11 @@ namespace RouteableTiles.API
             return db.Get(new[] {tile}).Select(x => x.osmGeo).GetRouteableTile(
                 isRelevant, keys => db.Get(keys, buffer).Select(x => x.osmGeo));
         }
+
+        internal static IEnumerable<OsmGeo> GetTile(this OsmTiledDbBase db, (uint x, uint y) tile,
+            byte[]? buffer = null)
+        {
+            return db.Get(new[] {tile}).Select(x => x.osmGeo);
+        }
     }
 }
